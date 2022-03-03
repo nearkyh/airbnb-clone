cb55e69650a2e77c0a7ed497b77d23786ade8cb8
Yonghan Kim <nearkyh@naver.com>
2022-03-03 21:02:27 +0900

    Feat: Users App 추가

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Custom User Model"""
+
+    GENDERS = (
+        ("M", "Male"),
+        ("W", "Female"),
+    )
+    LANGUAGES = (
+        ("eng", "English"),
+        ("kor", "Korean"),
+    )
+    CURRENCIES = (
+        ("usd", "USD"),
+        ("krw", "KRW"),
+    )
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDERS, max_length=10, blank=True)
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGES, max_length=10, blank=True)
+    currency = models.CharField(choices=CURRENCIES, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
