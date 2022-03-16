fc0cbb15848b2132c98aede1e27c047fecb56547
Yonghan Kim <nearkyh@naver.com>
2022-03-16 19:31:06 +0900

    View & Template 초기 구성

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("", include("core.urls", namespace="core")),
     path("admin/", admin.site.urls),
 ]
 
