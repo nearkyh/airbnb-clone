a18f82a6b8134637937a5bbc7c16a9551a2143b9
Yonghan Kim <nearkyh@naver.com>
2022-03-24 02:27:49 +0900

    Add rooms search view & template

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 app_name = "rooms"
 
-urlpatterns = [path("<int:rename_pk>", views.RoomDetail.as_view(), name="detail")]
+urlpatterns = [
+    path("<int:rename_pk>", views.RoomDetail.as_view(), name="detail"),
+    path("search/", views.search, name="search"),
+]
