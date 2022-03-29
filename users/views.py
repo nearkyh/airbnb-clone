98854d3a3bc16690971cce1b173cc320ba73381a
Yonghan Kim <nearkyh@naver.com>
2022-03-30 00:56:25 +0900

    Add login, logout view & template

--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
+from django.views import View
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import reverse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from . import forms
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        form = forms.LoginForm(initial={"email": "nearkyh@gmail.com"})
+        return render(request, "users/login.html", {"form": form})
+
+    def post(self, request):
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
+        return render(request, "users/login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse("core:home"))
