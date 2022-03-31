bcad6f599fe3963ed1fadedf4729b7c6e10b2a6c
Yonghan Kim <nearkyh@naver.com>
2022-04-01 00:34:03 +0900

    Add sign up view & template

--- users/views.py
+++ users/views.py
@@ -1,7 +1,5 @@
-from django.views import View
 from django.views.generic import FormView
 from django.urls import reverse_lazy
-from django.shortcuts import render
 from django.shortcuts import redirect
 from django.shortcuts import reverse
 from django.contrib.auth import authenticate
@@ -29,3 +27,24 @@ class LoginView(FormView):
 def logout_view(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "Yonghan",
+        "last_name": "Kim",
+        "email": "nearkyh2@gmail.com",
+    }
+
+    def form_valid(self, form):
+        form.save()
+        # Login
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
