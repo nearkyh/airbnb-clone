dad0d1fdb4a12244d3e4e947b02391eb299805fa
Yonghan Kim <nearkyh@naver.com>
2022-04-01 01:34:37 +0900

    Modify sign up view & template based ModelForm

--- users/models.py
+++ users/models.py
@@ -5,23 +5,33 @@ from django.db import models
 class User(AbstractUser):
     """Custom User Model"""
 
+    GENDER_MALE = "M"
+    GENDER_FEMALE = "W"
     GENDERS = (
-        ("M", "Male"),
-        ("W", "Female"),
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
     )
+    LANGUAGE_ENGLISH = "eng"
+    LANGUAGE_KOREAN = "kor"
     LANGUAGES = (
-        ("eng", "English"),
-        ("kor", "Korean"),
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
     )
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
     CURRENCIES = (
-        ("usd", "USD"),
-        ("krw", "KRW"),
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
     )
 
     avatar = models.ImageField(upload_to="avatars", blank=True)
     gender = models.CharField(choices=GENDERS, max_length=10, blank=True)
     bio = models.TextField(default="", blank=True)
     birthdate = models.DateField(blank=True, null=True)
-    language = models.CharField(choices=LANGUAGES, max_length=10, blank=True)
-    currency = models.CharField(choices=CURRENCIES, max_length=3, blank=True)
+    language = models.CharField(
+        choices=LANGUAGES, max_length=10, blank=True, default=LANGUAGE_KOREAN
+    )
+    currency = models.CharField(
+        choices=CURRENCIES, max_length=3, blank=True, default=CURRENCY_KRW
+    )
     superhost = models.BooleanField(default=False)
